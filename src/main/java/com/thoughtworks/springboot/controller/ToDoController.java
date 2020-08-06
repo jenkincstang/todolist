58be00378b141bbffa9d978289599a7a7fa592ec
jenkin <jenkincs@163.com>
2020-08-07 01:00:27 +0800

    [todolist][jenkin] feature: pass findAllToDos in controller

--- src/main/java/com/thoughtworks/springboot/controller/ToDoController.java
+++ src/main/java/com/thoughtworks/springboot/controller/ToDoController.java
@@ -1,7 +1,23 @@
 package com.thoughtworks.springboot.controller;
 
+import com.thoughtworks.springboot.entity.ToDo;
+import com.thoughtworks.springboot.service.ToDoService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class ToDoController {
+
+    private final ToDoService toDoService;
+
+    public ToDoController(ToDoService toDoService) {
+        this.toDoService = toDoService;
+    }
+
+    @GetMapping(value = "/todos")
+    public List<ToDo> findAllToDos() {
+        return toDoService.getAllToDos();
+    }
 }
