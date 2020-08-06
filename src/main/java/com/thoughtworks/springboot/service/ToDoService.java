94145794f1d7066bc64a2da175caeed5a2b096ba
jenkincstang <jenkincs@163.com>
2020-08-06 21:05:52 +0800

    [todo backend][jenkin,caps] feature: add methods to service

--- src/main/java/com/thoughtworks/springboot/service/ToDoService.java
+++ src/main/java/com/thoughtworks/springboot/service/ToDoService.java
@@ -1,5 +1,15 @@
 package com.thoughtworks.springboot.service;
 
-public interface ToDoService {
+import com.thoughtworks.springboot.entity.ToDo;
+import com.thoughtworks.springboot.repository.ToDoRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+public interface ToDoService {
+    List<ToDo> getAllToDos();
+    ToDo getToDoById(Integer id);
+    void deleteToDoById(Integer id);
+    ToDo saveToDo(ToDo toDo);
 }
--- src/main/java/com/thoughtworks/springboot/service/ToDoServiceImpl.java
+++ src/main/java/com/thoughtworks/springboot/service/ToDoServiceImpl.java
@@ -1,7 +1,27 @@
 package com.thoughtworks.springboot.service;
 
-import org.springframework.stereotype.Service;
+import com.thoughtworks.springboot.entity.ToDo;
 
-@Service
+import java.util.List;
+
 public class ToDoServiceImpl implements ToDoService {
+    @Override
+    public List<ToDo> getAllToDos() {
+        return null;
+    }
+
+    @Override
+    public ToDo getToDoById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public void deleteToDoById(Integer id) {
+
+    }
+
+    @Override
+    public ToDo saveToDo(ToDo toDo) {
+        return null;
+    }
 }
